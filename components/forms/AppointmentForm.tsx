a386cd604646e65ae76a4833511db8b82c7b37e0
AqibMiah <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>
2024-09-18 00:14:25 +0100

    Created the admin dashboard with full functionality. Appointments can be ammended and the data for all appointments can be shown.

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -18,14 +18,20 @@ import { Doctors } from "@/constants"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
 import { getAppointmentSchema } from "@/lib/validation";
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
+import { stat } from "fs"
  
  
-const AppointmentForm = ({userId, patientId, type 
+const AppointmentForm = ({userId, patientId, type, appointment, setOpen 
     }: {
     userId: string; 
     patientId: string; 
-    type: "create" | "cancel" | "schedule"}) => {
+    type: "create" | "cancel" | "schedule";
+    appointment?: Appointment;
+    setOpen: (open: boolean) => void;
+
+    }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false)
 
@@ -35,11 +41,11 @@ const AppointmentForm = ({userId, patientId, type
 const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-        primaryPhysician: "",
-        schedule: new Date(),
-        reason: "",
-        note: "",
-        cancellationReason: "",
+        primaryPhysician: appointment ? appointment.primaryPhysician : '',
+        schedule: appointment ? new Date(appointment?.schedule) : new Date(Date.now()),
+        reason: appointment ? appointment.reason : '',
+        note: appointment?.note || '',
+        cancellationReason: appointment?.cancellationReason || '',
     },
 })
  
@@ -63,7 +69,6 @@ const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     }
 
     try {
-        console.log()
         if(type === 'create' && patientId) {
             const appointmentData = {
                 userId,
@@ -80,6 +85,25 @@ const form = useForm<z.infer<typeof AppointmentFormValidation>>({
             if(appointment) {
                 form.reset();
                 router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
+            }
+        } else{
+            const appointmentToUpdate = {
+                userId,
+                appointmentId: appointment?.$id!,
+                appointment: {
+                    primaryPhysician: values?.primaryPhysician,
+                    schedule: new Date(values?.schedule),
+                    status: status as Status,
+                    cancellationReason: values?.cancellationReason,
+                },
+                type
+            }
+
+            const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+            if(updatedAppointment){
+                setOpen && setOpen(false);
+                form.reset();
             }
         }
         
@@ -106,10 +130,10 @@ const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     return (
         <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <section className="mb-12 space-y-4">
+          {type === 'create' && <section className="mb-12 space-y-4">
             <h1 className="header"> Hi there</h1>
             <p className="text-dark-700">Schedule your first appointment</p>
-            </section>
+            </section>}
 
             {type !== 'cancel' && (
                 <>
